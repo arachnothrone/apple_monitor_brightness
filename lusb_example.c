e391448c20b24f25cf5fa992ec62adce56b93dba
arachnothrone <js1000avf@gmail.com>
2020-05-09 21:31:51 -0400

    Add get device list

--- lusb_example.c
+++ lusb_example.c
@@ -13,28 +13,74 @@ static struct libusb_device_handle *devh = NULL;
 static int find_dpfp_device(void)
 {
 	devh = libusb_open_device_with_vid_pid(NULL, 0x05ac, 0x9226);
+	printf("devh: %0x\n", (unsigned int) devh);
 	return devh ? 0 : -EIO;
+}
+
+static void print_devs(libusb_device **devs)
+{
+	libusb_device *dev;
+	int i = 0, j = 0;
+	uint8_t path[8]; 
+
+	while ((dev = devs[i++]) != NULL) {
+		struct libusb_device_descriptor desc;
+		int r = libusb_get_device_descriptor(dev, &desc);
+		if (r < 0) {
+			fprintf(stderr, "failed to get device descriptor");
+			return;
+		}
+
+		printf("%04x:%04x (bus %d, device %d)",
+			desc.idVendor, desc.idProduct,
+			libusb_get_bus_number(dev), libusb_get_device_address(dev));
+
+		r = libusb_get_port_numbers(dev, path, sizeof(path));
+		if (r > 0) {
+			printf(" path: %d", path[0]);
+			for (j = 1; j < r; j++)
+				printf(".%d", path[j]);
+		}
+		printf("\n");
+	}
 }
 
 int main(void)
 {
-    int Var1 = 0x55;
-    printf("Start main: \n");
+	printf("Start main: \n");
 
     struct sigaction sigact;
 	int r;
+	libusb_device **devs;
+	ssize_t cnt;
 
 	r = libusb_init(NULL);
+	printf("1. INIT: %d\n", r);
 	if (r < 0) {
 		fprintf(stderr, "failed to initialise libusb\n");
 		exit(1);
 	}
 
-    r = find_dpfp_device();
-	if (r < 0) {
-		fprintf(stderr, "Could not find/open device\n");
-		// goto out;
-	}
+	cnt = libusb_get_device_list(NULL, &devs);
+	printf("1a. List: %ld\n", cnt);
+
+	print_devs(devs);
+	libusb_free_device_list(devs, 1);
+
+    // r = find_dpfp_device();
+	// printf("2. FIND: %d\n", r);
+	// if (r < 0) {
+	// 	fprintf(stderr, "Could not find/open device\n");
+	// 	// goto out;
+	// }
+
+	// r = libusb_claim_interface(devh, 0);
+	// printf("3. CLAIM: %d\n", r);
+	// if (r < 0) {
+	// 	fprintf(stderr, "usb_claim_interface error %d\n", r);
+	// }
+	// printf("claimed interface\n");
+
 
     return 0;
 }
